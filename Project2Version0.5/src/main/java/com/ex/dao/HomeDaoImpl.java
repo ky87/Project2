d493d9be23d2f9507fa9f57fbe46022aa718fa97
unknown <jakpala@gmail.com>
2018-07-29 21:53:30 -0400

    Version0.5

---
+++ Project2Version0.5/src/main/java/com/ex/dao/HomeDaoImpl.java
@@ -0,0 +1,55 @@
+package com.ex.dao;
+
+
+import com.ex.model.Court;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository
+public class HomeDaoImpl implements BaseDao<Court> {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    @Override
+    public List<Court> findAll() {
+        String hql = "From Court";
+
+        // Session session = sessionFactory.openSession();
+        //session.beginTransaction();
+        //String sql = "select * from profile";
+        // int result =  session.createSQLQuery(sql).getFirstResult();
+        //session.getTransaction().commit();
+        //session.close();
+
+        Query query = sessionFactory.openSession().createQuery(hql);
+        return  query.list();
+    }
+
+    @Override
+    public Court findOne(Serializable id) {
+
+
+        return null;
+    }
+
+    @Override
+    public Court update(Court o) {
+        return null;
+    }
+
+    @Override
+    public void delete(Serializable id) {
+
+    }
+}
---
+++ Project2Version0.5/src/main/java/com/ex/model/Player.java
@@ -0,0 +1,104 @@
+package com.ex.model;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="profile", schema="public")
+
+public class Player {
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name", nullable=false, unique=true)
+    private String name;
+
+    @Column(name="password", nullable=false)
+    private String password;
+
+    @Column(name="location", nullable=false)
+    private String location;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="age", nullable=false)
+    private int age;
+
+    @Column(name="tennis_rank", nullable=false)
+    private int tennis_rank;
+
+    public Player(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTennis_rank() {
+        return tennis_rank;
+    }
+
+    public void setTennis_rank(int tennis_rank) {
+        this.tennis_rank = tennis_rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", location='" + location + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", tennis_rank=" + tennis_rank +
+                '}';
+    }
+}
